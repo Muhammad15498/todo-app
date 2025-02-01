187cf8dc0464ff5c6e81f41e1bd8d3e121ab0b83
Muhammad15498 <medo1541998@gmail.com>
2025-02-01 11:08:29 +0200

    change 1

--- app.py
+++ app.py
@@ -301,4 +301,4 @@ def delete_note(note_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create the database and tables
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Use 0.0.0.0 for external access
